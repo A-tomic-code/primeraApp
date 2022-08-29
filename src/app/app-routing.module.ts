c686caa99c41f9dc11b658d683f41ea828d29a02
Daniel <danielvazquezheredia1994@gmail.com>
2022-08-29 16:51:46 +0200

    primera app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "header", component: HeaderComponent},
+  {path:"footer", component: FooterComponent},
+  {path:"registro", component: RegistroComponent},
+  {path: "", component: HomeComponent}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormularioRegistroComponent } from './component/formulario-registro/formulario-registro.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    FormularioRegistroComponent,
+    RegistroComponent,
   ],
   imports: [
     BrowserModule,
